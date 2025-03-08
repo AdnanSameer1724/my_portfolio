7df09b2c411bfe25f0f23b842cd123187a06c635
adnan50 <adnan@livekeeping.com>
2025-03-08 20:41:57 +0530

    Homepage initial work

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="w-full h-screen bg-[#1b1f24] text-white flex flex-col">
+    <div class="flex flex-col md:flex-row items-center justify-between pt-20">
+      <div class="w-full md:w-2/5">
+        <img
+          src="../src//assets/images/profile2.jpg"
+          alt="Profile Image"
+          class="w-full max-h-full object-contain"
+        />
+      </div>
+
+      <div class="w-full md:w-3/5 text-left md:pl-12">
+        <h2 class="text-5xl font-bold text-gray-400">
+          My <span class="text-white">Portfolio</span>
+        </h2>
+        <p class="py-10 mr-24">
+          A highly motivated and detail-oriented Frontend Web Developer with a
+          passion for creating responsive, user-friendly websites and
+          applications. Proficient in HTML, CSS, JavaScript, and modern frontend
+          frameworks like React and Vue.js. Adept at transforming design
+          concepts into seamless, interactive user experiences. Strong
+          problem-solving skills with a deep understanding of web performance,
+          accessibility, and cross-browser compatibility. Dedicated to
+          continuous learning and staying updated with industry trends to
+          deliver cutting-edge web solutions.
+        </p>
+        <button
+          class="bg-white text-xl text-[#1b1f24] px-4 py-2 border border-gray-300 cursor-pointer hover:bg-gray-100"
+        >
+          Contact
+        </button>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
       "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          modernBlack: "#1b1f24",
+        }
+      },
     },
     plugins: [],
   }
