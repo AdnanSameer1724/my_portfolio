88e97d0a3aa7bae845446c09a94a17d257c3112b
adnan50 <adnan@livekeeping.com>
2025-03-12 23:43:23 +0530

    changing template and setting left section

---
+++ src/components/LeftComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <aside
+    class="fixed left-10 bottom-10 top-10 w-1/5 bg-[#1f1f1f] border border-[#565656] rounded-2xl p-5 font-sans"
+  >
+    <h1 class="text-2xl text-white font-bold mb-[5px]">Adnan</h1>
+    <h1 class="text-2xl text-white font-bold mb-[30px]">Sameer Z</h1>
+
+    <img 
+        src="../assets/images/me.jpeg"
+        alt="me"
+        class="w-[250px] h-[250px] object-cover rounded-2xl"
+    />
+  </aside>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex min-h-screen bg-[#1f1f1f] text-white">
+        <!-- Left component -->
+         <LeftComponent />
+    </div>
+</template>
+
+<script setup>
+    import LeftComponent from '../components/LeftComponent.vue';
+</script>
