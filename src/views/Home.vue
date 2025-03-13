346bc4cdeb8d4f1ec6e6ec0c25ef605402aa4d9f
adnan50 <adnan@livekeeping.com>
2025-03-13 21:58:53 +0530

    left component assigning and initializing right component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
     <div class="flex min-h-screen bg-[#1f1f1f] text-white">
         <!-- Left component -->
          <LeftComponent />
+         <RightComponent />
     </div>
 </template>
 
 <script setup>
     import LeftComponent from '../components/LeftComponent.vue';
+    import RightComponent from '../components/RightComponent.vue';
 </script>
