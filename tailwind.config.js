7c2731130b4ef9f0fa7e212cf7c16a561caa42e7
adnan50 <adnan@livekeeping.com>
2025-03-14 19:35:12 +0530

    Right component icon nuilding

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ export default {
         }
       },
     },
-    plugins: [],
+    plugins: [
+      function ({ addComponents }) {
+        addComponents({
+          '.icon-gray': {
+            filter: 'invert(62%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(85%) contrast(90%)',
+            transition: 'filter 0.3s ease-in-out',
+          },
+          '.icon-gray:hover': {
+            filter: 'invert(69%) sepia(68%) saturate(447%) hue-rotate(98deg) brightness(93%) contrast(89%)',
+          },
+        });
+      },
+    ],
   }
   
