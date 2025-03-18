3a3f6afd84768ec7fb8996b128adc49911bd7103
adnan50 <adnan@livekeeping.com>
2025-03-18 22:46:33 +0530

    title adding

--- src/components/RightComponent.vue
+++ src/components/RightComponent.vue
@@ -7,43 +7,43 @@
     <aside v-if="isSideBarVisible" class="fixed right-10 bottom-45 top-40 w-1/33 bg-[#1f1f1f] border border-[#565656] px-2 py-5 rounded-4xl mt-2 flex flex-col items-center">
         <ul class="w-full space-y-5">
             <li>
-                <a href="#about" class="block text-center transition duration-300">
-                    <img :src="Home" alt="Home" class="w-6 h-6 icon-gray"/>
+                <a href="#about" class="block text-center transition duration-300" title="Home">
+                    <img :src="Home" alt="Home" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#projects" class="block text-center transition duration-300">
-                    <img :src="Intro" alt="Intro" class="w-6 h-6 icon-gray"/>
+                <a href="#intro" class="block text-center transition duration-300" title="About">
+                    <img :src="Intro" alt="Intro" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#education" class="block text-center transition duration-300">
-                    <img :src="Education" alt="Education" class="w-6 h-6 icon-gray"/>
+                <a href="#education" class="block text-center transition duration-300" title="Education">
+                    <img :src="Education" alt="Education" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#experience" class="block text-center transition duration-300">
-                    <img :src="Experience" alt="Experience" class="w-6 h-6 icon-gray"/>
+                <a href="#experience" class="block text-center transition duration-300" title="Experience">
+                    <img :src="Experience" alt="Experience" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#services" class="block text-center transition duration-300">
-                    <img :src="Services" alt="Services" class="w-6 h-6 icon-gray"/>
+                <a href="#services" class="block text-center transition duration-300" title="Services">
+                    <img :src="Services" alt="Services" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#skills" class="block text-center transition duration-300">
-                    <img :src="Skills" alt="Skills" class="w-6 h-6 icon-gray"/>
+                <a href="#skills" class="block text-center transition duration-300" title="Skills">
+                    <img :src="Skills" alt="Skills" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#projects" class="block text-center transition duration-300">
-                    <img :src="Projects" alt="Projects" class="w-6 h-6 icon-gray"/>
+                <a href="#projects" class="block text-center transition duration-300" title="Projects">
+                    <img :src="Projects" alt="Projects" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
             <li>
-                <a href="#contact" class="block text-center transition duration-300">
-                    <img :src="email" alt="email" class="w-6 h-6 icon-gray"/>
+                <a href="#contact" class="block text-center transition duration-300" title="Contact">
+                    <img :src="email" alt="email" class="w-6 h-6 object-contain icon-gray"/>
                 </a>
             </li>
         </ul>
